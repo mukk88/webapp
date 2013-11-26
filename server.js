004f4d8cb08d225466b390a57345fb08d400285a
mukk88 <markwoo@gmail.com>
2013-11-25 17:30:29 -0700

    trying old server code

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
-
-/**
- * Module dependencies
- */
-
-var express = require('express'),
+var express = require('express');
     routes = require('./routes'),
     api = require('./routes/api'),
     http = require('http'),
     path = require('path');
 
 var app = module.exports = express();
-// app.http().io();
 
-
-/**
- * Configuration
- */
-
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 // app.set('view engine', 'jade');
@@ -38,46 +26,25 @@ if (app.get('env') === 'production') {
   // TODO
 };
 
-
 /**
  * Routes
  */
 
 // serve index and view partials
 app.get('/', routes.index);
-app.get('/splash.html', routes.splash);
 // app.get('/partials/:name', routes.partials);
 
 // JSON API
+// app.get('/api/name', api.name);
 app.get('/api/createGame', api.createGame);
-app.get('/api/getAllGames', api.getAllGames);
 app.get('/api/getCards', api.getCards);
 app.post('/api/updateCards', api.updateCards);
-// app.post('/login', passport.authenticate('local', { successRedirect: '/',
-//                                                     failureRedirect: '/login' }));
+app.get('/api/getUpdatedCards', api.getUpdatedCards);
 
 // redirect all others to the index (HTML5 history)
-// app.get('*', routes.index);
+app.get('*', routes.index);
 
 
-// express.io stuff
-app.io.route('updateCards', function(req) {
-    req.io.room(req.data.gid).broadcast('cardsUpdated', {message: req.data.card});
-})
-
-app.io.route('join', function(req) {
-    req.io.join(req.data);
-    console.log('done joining');
-})
-
-// app.get('/', function(req, res) {
-//     res.sendfile(__dirname + '/views/index.html');
-// })
-
-/**
- * Start Server
- */
-
-// http.createServer(app).listen(app.get('port'), function () {
+http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
