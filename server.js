201fe14c65c4b83e6d611346a642e3a260bc3b68
mukk88 <markwoo@gmail.com>
2013-11-25 17:35:13 -0700

    trying old server code4

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// var express = require('express');
-//     // routes = require('./routes'),
-//     // api = require('./routes/api'),
-//     http = require('http'),
-//     path = require('path');
+var express = require('express');
+    // routes = require('./routes'),
+    // api = require('./routes/api'),
+    http = require('http'),
+    path = require('path');
 
 // var app = module.exports = express();
 
@@ -53,7 +53,6 @@
 // });
 
 
-var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
