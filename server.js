3c43183ed6350cd2d3f8712a0f4966a2ad9a308a
mukk88 <markwoo@gmail.com>
2013-11-25 18:13:17 -0700

    trying server4

--- server.js
+++ server.js
@@ -6,44 +6,44 @@ var express = require('express.io'),
 
 var app = module.exports = express();
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-// app.set('view engine', 'jade');
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
-app.engine('html', require('ejs').renderFile);
+// app.set('port', process.env.PORT || 3000);
+// app.set('views', __dirname + '/views');
+// // app.set('view engine', 'jade');
+// app.use(express.logger('dev'));
+// app.use(express.bodyParser());
+// app.use(express.methodOverride());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(app.router);
+// app.engine('html', require('ejs').renderFile);
 
-// development only
-if (app.get('env') === 'development') {
-  app.use(express.errorHandler());
-}
+// // development only
+// if (app.get('env') === 'development') {
+//   app.use(express.errorHandler());
+// }
 
-// production only
-if (app.get('env') === 'production') {
-  // TODO
-};
+// // production only
+// if (app.get('env') === 'production') {
+//   // TODO
+// };
 
-/**
- * Routes
- */
+// /**
+//  * Routes
+//  */
 
-// serve index and view partials
-app.get('/', routes.index);
-// app.get('/partials/:name', routes.partials);
+// // serve index and view partials
+// app.get('/', routes.index);
+// // app.get('/partials/:name', routes.partials);
 
-// JSON API
-// app.get('/api/name', api.name);
+// // JSON API
+// // app.get('/api/name', api.name);
 
-app.get('/api/createGame', api.createGame);
-app.get('/api/getCards', api.getCards);
-app.post('/api/updateCards', api.updateCards);
-app.get('/api/getUpdatedCards', api.getUpdatedCards);
+// app.get('/api/createGame', api.createGame);
+// app.get('/api/getCards', api.getCards);
+// app.post('/api/updateCards', api.updateCards);
+// app.get('/api/getUpdatedCards', api.getUpdatedCards);
 
-// redirect all others to the index (HTML5 history)
-app.get('*', routes.index);
+// // redirect all others to the index (HTML5 history)
+// app.get('*', routes.index);
 
 
 // app.listen(app.get('port'), function () {
