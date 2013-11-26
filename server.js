fc319bb1c91ed5c4c20677179e5cb03dd9fe10cb
mukk88 <markwoo@gmail.com>
2013-11-25 18:06:45 -0700

    trying server2

--- server.js
+++ server.js
@@ -6,55 +6,53 @@ var express = require('express.io'),
 
 var app = module.exports = express();
 
-// app.set('port', process.env.PORT || 3000);
-// app.set('views', __dirname + '/views');
-// // app.set('view engine', 'jade');
-// app.use(express.logger('dev'));
-// app.use(express.bodyParser());
-// app.use(express.methodOverride());
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(app.router);
-// app.engine('html', require('ejs').renderFile);
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+// app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+app.engine('html', require('ejs').renderFile);
 
-// // development only
-// if (app.get('env') === 'development') {
-//   app.use(express.errorHandler());
-// }
+// development only
+if (app.get('env') === 'development') {
+  app.use(express.errorHandler());
+}
 
-// // production only
-// if (app.get('env') === 'production') {
-//   // TODO
-// };
+// production only
+if (app.get('env') === 'production') {
+  // TODO
+};
 
-// /**
-//  * Routes
-//  */
+/**
+ * Routes
+ */
 
-// // serve index and view partials
-// app.get('/', routes.index);
-// // app.get('/partials/:name', routes.partials);
+// serve index and view partials
+app.get('/', routes.index);
+// app.get('/partials/:name', routes.partials);
 
-// // JSON API
-// // app.get('/api/name', api.name);
+// JSON API
+// app.get('/api/name', api.name);
 
+app.get('/api/createGame', api.createGame);
+app.get('/api/getCards', api.getCards);
+app.post('/api/updateCards', api.updateCards);
+app.get('/api/getUpdatedCards', api.getUpdatedCards);
 
-
-// // app.get('/api/createGame', api.createGame);
-// // app.get('/api/getCards', api.getCards);
-// // app.post('/api/updateCards', api.updateCards);
-// // app.get('/api/getUpdatedCards', api.getUpdatedCards);
-
-// // redirect all others to the index (HTML5 history)
-// app.get('*', routes.index);
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
 
 
-// http.createServer(app).listen(app.get('port'), function () {
-//   console.log('Express server listening on port ' + app.get('port'));
-// });
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+// var port = process.env.PORT || 1337;
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World\n');
+// }).listen(port);
