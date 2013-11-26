7db417d2e50c8632ff25839b1041d6463777aa52
mukk88 <markwoo@gmail.com>
2013-11-25 21:55:29 -0700

    updating index.js

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,7 @@ exports.index = function(req, res){
   res.render('index.html');
 };
 
-<<<<<<< HEAD
-exports.splash = function(req,res){
-	res.render('splash.html');
-}
 
-// exports.partials = function (req, res) {
-//   var name = req.params.name;
-//   res.render('partials/' + name);
-// };
-=======
 exports.splash = function(req, res){
   res.render('splash.html');
 };
@@ -32,5 +23,4 @@ exports.authFacebookCallback = function(req, res){
 exports.logout = function(req, res){
   req.logout();
   res.redirect('/');
-};
->>>>>>> 6cb7904e0ed2a54e13222e3472b0c37bfabd2d4d
+};s
