7b3446927464382823d025652f652e925af68a01
mukk88 <markwoo@gmail.com>
2013-11-25 18:09:58 -0700

    trying server3

--- server.js
+++ server.js
@@ -46,7 +46,12 @@ app.get('/api/getUpdatedCards', api.getUpdatedCards);
 app.get('*', routes.index);
 
 
-app.listen(app.get('port'), function () {
+// app.listen(app.get('port'), function () {
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
+
+
+http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
