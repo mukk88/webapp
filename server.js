f1fcd01fc8876b77c964e4345b62a1f04079c6c2
mukk88 <markwoo@gmail.com>
2013-11-25 17:32:36 -0700

    trying without express2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-    routes = require('./routes'),
-    api = require('./routes/api'),
+    // routes = require('./routes'),
+    // api = require('./routes/api'),
     http = require('http'),
     path = require('path');
 
@@ -36,10 +36,13 @@ app.get('/', routes.index);
 
 // JSON API
 // app.get('/api/name', api.name);
-app.get('/api/createGame', api.createGame);
-app.get('/api/getCards', api.getCards);
-app.post('/api/updateCards', api.updateCards);
-app.get('/api/getUpdatedCards', api.getUpdatedCards);
+
+
+
+// app.get('/api/createGame', api.createGame);
+// app.get('/api/getCards', api.getCards);
+// app.post('/api/updateCards', api.updateCards);
+// app.get('/api/getUpdatedCards', api.getUpdatedCards);
 
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
