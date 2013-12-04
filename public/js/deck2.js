2a719f39c845d54d3903e990969a20e064857334
mukk88 <markwoo@gmail.com>
2013-12-03 21:51:19 -0700

    fixing image bug

--- public/js/deck2.js
+++ public/js/deck2.js
@@ -7,7 +7,7 @@ var l = $(window).width()/2-260;
 console.log(t);
 var counter=0;
 for(var i=0;i<10;i++){
-    document.write('<img id = "' + i + 's" class = "draggable" height = 246 width = 175 back="images/g'+ i +'.jpg" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/back.jpg">');
+    document.write('<img id = "' + i + 's" class = "draggable" height = 246 width = 175 back="images/creativeimages/g'+ i +'.jpg" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/back.jpg">');
     // document.write('<img id = "' + i + 'h" class = "draggable" height = 246 width = 175 back="images/'+ i +'h.jpg" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/back.jpg">');
     // document.write('<img id = "' + i + 'c" class = "draggable" height = 246 width = 175 back="images/'+ i +'c.jpg" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/back.jpg">');
     // document.write('<img id = "' + i + 'd" class = "draggable" height = 246 width = 175 back="images/'+ i +'d.jpg" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/back.jpg">');
