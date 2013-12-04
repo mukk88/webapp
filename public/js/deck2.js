4d35be61b951a463abf7e36d026f1a5930b37fe7
mukk88 <markwoo@gmail.com>
2013-12-03 21:55:39 -0700

    adding remaining images

--- public/js/deck2.js
+++ public/js/deck2.js
@@ -7,9 +7,9 @@ var l = $(window).width()/2-260;
 console.log(t);
 var counter=0;
 for(var i=0;i<10;i++){
-    document.write('<img id = "' + i + 's" class = "draggable" height = 246 width = 175 back="images/creativeimages/g'+ i +'.jpg" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/back.jpg">');
-    // document.write('<img id = "' + i + 'h" class = "draggable" height = 246 width = 175 back="images/'+ i +'h.jpg" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/back.jpg">');
-    // document.write('<img id = "' + i + 'c" class = "draggable" height = 246 width = 175 back="images/'+ i +'c.jpg" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/back.jpg">');
-    // document.write('<img id = "' + i + 'd" class = "draggable" height = 246 width = 175 back="images/'+ i +'d.jpg" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/back.jpg">');
+    document.write('<img id = "' + i + 's" class = "draggable" height = 246 width = 175 back="images/creativeimages/g'+ i +'.png" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/creativeimages/back.jpg">');
+    document.write('<img id = "' + i + 's" class = "draggable" height = 246 width = 175 back="images/creativeimages/r'+ i +'.png" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/creativeimages/back.jpg">');
+    document.write('<img id = "' + i + 's" class = "draggable" height = 246 width = 175 back="images/creativeimages/c'+ i +'.png" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/creativeimages/back.jpg">');
+    document.write('<img id = "' + i + 's" class = "draggable" height = 246 width = 175 back="images/creativeimages/a'+ i +'.png" style="top:' + (t+counter) + 'px; left:' + (l+counter) + 'px;" src="images/creativeimages/back.jpg">');
     counter++;
 }
