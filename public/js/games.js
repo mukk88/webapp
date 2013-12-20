a150171384e9ff42bb99b0a429a074241a5f9de8
mukk88 <markwoo@gmail.com>
2013-12-21 07:34:28 +0800

    updating role

--- public/js/games.js
+++ public/js/games.js
@@ -63,7 +63,7 @@ function GameCtrl($scope) {
 				var oldGames = $scope.games;
 			    $scope.games = [];
 			    angular.forEach(oldGames, function(game) {
-			      if (game.gid!=gid) $scope.gamess.push(game);
+			      if (game.gid!=gid) $scope.games.push(game);
 			    });
 			}else{
 				$('#input' + gid).val('');
