36b514d0f740e74abef2b6cb54aff21c16ec43e2
Mark Woo Yong Ming <wooym2@skinner.cs.byu.edu>
2014-01-24 15:56:29 -0700

    about

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
 	console.log(h)
 	$('.story').height(h);
 	$('.story-2').height(h);
+
+	$('#navs > li').each(function(index){
+		console.log(index);
+		$(this).click(function(){
+			$('html,body').animate({scrollTop: (index*h)}, 1000);
+		});
+	});
+
+	
 })
