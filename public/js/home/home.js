af4107f72de663270b67cac5fd54319edafde4db
mukk88 <markwoo@gmail.com>
2014-01-25 14:32:32 -0700

    reworking home page

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -1,16 +1,31 @@
 
 $(document).ready(function() {
 	var h = window.innerHeight;
-	console.log(h)
-	$('.story').height(h);
-	$('.story-2').height(h);
+	var w = $(window).width();
+	console.log(w);
+	if(w>h){
+		$('.story').height(h);
+		$('.story-2').height(h);
+	}else{
+		$('.story').height(1000);
+		$('.story-2').height(1000);		
+	}
 
+	$(window).scroll(function(){
+		$('li').css('color','black');
+		var curheight = $(this).scrollTop();
+		for(var i =0;i<5;i++){
+			if(curheight < (i+1)*h){
+				$('#li' + i ).css('color','#BAA378');
+				break;
+			}
+		}
+	})
 	$('#navs > li').each(function(index){
-		console.log(index);
 		$(this).click(function(){
 			$('html,body').animate({scrollTop: (index*h)}, 1000);
 		});
 	});
 
-	
+	$('#li0').css('color','#BAA378');	
 })
